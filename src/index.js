187153bd6a10cf6d4ebc9d1b08164c474833f2a3
KelvinYou <ykekelvin0220@gmail.com>
2023-01-05 19:27:21 +0800

    setup gh-pages

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { TransactionsProvider } from "./context/TransactionContext";
-ReactDOM.render(
-    <TransactionsProvider>
-    <App />
-    </TransactionsProvider>,
-  document.getElementById('root')
+import { HashRouter as Router } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <Router>
+      <TransactionsProvider>
+      <App />
+      </TransactionsProvider>,
+    </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
